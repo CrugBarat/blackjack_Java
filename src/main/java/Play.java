899f169a7de948a9488ab765b152a5dfe27c7d2e
CrugBarat <craigtbarratt@gmail.com>
2020-05-31 18:13:14 +0100

    deals cards to user in console
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

--- src/main/java/Play.java
+++ src/main/java/Play.java
@@ -8,11 +8,16 @@ public class Play {
         System.out.println("\n");
 
         Player player = new Player("");
+        Dealer dealer = new Dealer("Maverick");
+        GameTable gametable = new GameTable();
+        Deck deck = new Deck();
+        BlackJack blackJack = new BlackJack(gametable, dealer, deck);
 
         System.out.println("Please enter your name:");
         Scanner userName = new Scanner(System.in);
         String name = userName.next();
         player.setName(name);
+        gametable.addPlayer(player);
 
         System.out.println("\n");
         System.out.println("Welcome " + player.getName() + "!");
@@ -25,5 +30,13 @@ public class Play {
 
         System.out.println("\n");
         System.out.println("Great! " + player.getName() + " you have " + player.getCash() + " bucks!");
+        System.out.println("Dealing....");
+
+        blackJack.deal();
+
+        System.out.println("Your hand: " + player.handTotal());
+        System.out.println("Dealers hand: " + dealer.handTotal());
+
+
     }
 }
