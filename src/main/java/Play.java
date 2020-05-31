fcf1eac90eac9c9077fbd9defb521d8210708c21
CrugBarat <craigtbarratt@gmail.com>
2020-05-31 17:52:16 +0100

    gets users cash from console
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

--- src/main/java/Play.java
+++ src/main/java/Play.java
@@ -5,15 +5,25 @@ public class Play {
     public static void main(String[] args) {
 
         System.out.println("Welcome to BlackJack!");
+        System.out.println("\n");
 
         Player player = new Player("");
 
         System.out.println("Please enter your name:");
-        Scanner userInput = new Scanner(System.in);
-        String name = userInput.next();
+        Scanner userName = new Scanner(System.in);
+        String name = userName.next();
         player.setName(name);
+
+        System.out.println("\n");
         System.out.println("Welcome " + player.getName() + "!");
 
 
+        System.out.println("How much cash do you have?");
+        Scanner userCash = new Scanner(System.in);
+        int cash = Integer.parseInt(userCash.next());
+        player.setCash(cash);
+
+        System.out.println("\n");
+        System.out.println("Great! " + player.getName() + " you have " + player.getCash() + " bucks!");
     }
 }
