ed14d2880348159f18433f67a655d3d5a4b69b94
CrugBarat <craigtbarratt@gmail.com>
2020-05-31 17:38:03 +0100

    gets users name from console
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

--- src/main/java/Play.java
+++ src/main/java/Play.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class Play {
 
     public static void main(String[] args) {
 
         System.out.println("Welcome to BlackJack!");
+
+        Player player = new Player("");
+
+        System.out.println("Please enter your name:");
+        Scanner userInput = new Scanner(System.in);
+        String name = userInput.next();
+        player.setName(name);
+        System.out.println("Welcome " + player.getName() + "!");
+
+
     }
 }
